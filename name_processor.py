0d0e5d828414e1caec6cb58648f82ba63c1d9431
sologuboved <sologuboved@gmail.com>
2017-07-07 14:38:38 +0300

    finishing off

--- name_processor.py
+++ name_processor.py
@@ -2,7 +2,12 @@
 
 import json
 
-NAMES_JSON = 'names_44.json'
+NAMES_44 = 'names_44.json'
+NAMEBASE_F = 'namebase_f.json'
+NAMEBASE_M = 'namebase_m.json'
+NAMES_F = 'names_f.json'
+NAMES_M = 'names_m.json'
+UNCLEAR = 'unclear.json'
 
 
 def load_json(json_file):
@@ -10,20 +15,105 @@ def load_json(json_file):
         return json.load(data)
 
 
-def compile_freq_dict(names):
-    freq_dict = dict()
+def dump_json(names, json_file):
+    with open(json_file, 'w') as handler:
+        json.dump(names, handler)
+
+
+def clean_names(names):
+    return [name[0].upper() + name[1:].lower() for name in names]
+
+
+def group_names(names_file, namebase_f_file, namebase_m_file, names_f_file, names_m_file, unclear_file):
+    names = load_json(names_file)
+    namebase_f = load_json(namebase_f_file)
+    namebase_m = load_json(namebase_m_file)
+
+    names_f = list()
+    names_m = list()
+    unclear = list()
+
     for name in names:
-        freq_dict[name.lower()] = freq_dict.get(name.lower(), 0) + 1
+        if name in namebase_f:
+            names_f.append(name)
+        elif name in namebase_m:
+            names_m.append(name)
+        else:
+            unclear.append(name)
+
+    print "Dumping female names..."
+    dump_json(names_f, names_f_file)
+    print "Dumping male names..."
+    dump_json(names_m, names_m_file)
+    print "Dumping names of unclear gender..."
+    dump_json(unclear, unclear_file)
+    print
+    print
+
+    # print 'names_f'
+    # print len(names_f)
+    # for name in names_f[:10]:
+    #     print name
+    # print
+    # print
+    # print 'names_m'
+    # print len(names_m)
+    # for name in names_m[:10]:
+    #     print name
+    # print
+    # print
+    # print len(unclear)
+    # for name in unclear:
+    #     print name
+
+
+def compile_freq_dict(names, fem=False):
+    freq_dict = dict()
+
+    for name in names:
+        freq_dict[name] = freq_dict.get(name, 0) + 1
+
+    if fem:
+        freq_dict[u'Наталья / Наталия'] = freq_dict[u'Наталья'] + freq_dict[u'Наталия']
+        freq_dict.pop(u'Наталья', None)
+        freq_dict.pop(u'Наталия', None)
+
+        freq_dict[u'Софья / София'] = freq_dict[u'Софья'] + freq_dict[u'София']
+        freq_dict.pop(u'Софья', None)
+        freq_dict.pop(u'София', None)
+
     return freq_dict
 
 
 def sort_freq_dict(freq_dict):
-    names = freq_dict.items()
-    return sorted(names, key=lambda n: n[1], reverse=True)
+    return sorted(freq_dict.items(), key=lambda n: n[1], reverse=True)
+
 
 if __name__ == '__main__':
-    all_names = load_json(NAMES_JSON).values()
-    first20 = sort_freq_dict(compile_freq_dict(all_names[:]))
-    for each_name, freq in first20:
-        print each_name, freq
 
+    # group_names(NAMES_44, NAMEBASE_F, NAMEBASE_M, NAMES_F, NAMES_M, UNCLEAR)
+
+    print "FREQUENCIES: FEMALE",
+    sorted_f = sort_freq_dict(compile_freq_dict(load_json(NAMES_F), fem=True))
+    print "[%d names, %d entries]" % (len(sorted_f), sum([entry[1] for entry in sorted_f]))
+    print
+    for n, freq in sorted_f:
+        print n + ':', freq
+    print
+    print
+
+    print "FREQUENCIES: MALE",
+    sorted_m = sort_freq_dict(compile_freq_dict(load_json(NAMES_M)))
+    print "[%d names, %d entries]" % (len(sorted_m), sum([entry[1] for entry in sorted_m]))
+    print
+    for n, freq in sorted_m:
+        print n + ':', freq
+    print
+    print
+
+    print "FREQUENCIES: UNCLEAR",
+    sorted_unclear = sort_freq_dict(compile_freq_dict(load_json(UNCLEAR)))
+    print "[%d names, %d entries; showing first 20]" % (len(sorted_unclear), sum([entry[1] for entry in sorted_unclear]))
+    print
+    for n, freq in sorted_unclear[: 20]:
+        print n + ':', freq
---
+++ obtain_namebase.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+URL_F = 'http://kakzovut.ru/woman.html'
+URL_M = 'http://kakzovut.ru/man.html'
+NAMEBASE_F = 'namebase_f.json'
+NAMEBASE_M = 'namebase_m.json'
+
+
+def scrape_names(url):
+    soup = BeautifulSoup(requests.get(url).content)
+    return [name.find_all('a')[0].text for name in soup.find_all('div', {'class': 'nameslist'})]
+
+
+def amplify_names(names, fem=False):
+
+    amplified = list()
+
+    for name in names:
+        amplified.append(name)
+        if u'ё' in name:
+            amplified.append(substitute_ye_for_yo(name))
+
+    if fem:
+        amplified.extend([u'Наталия', u'София'])
+
+    else:
+        amplified.extend([u'Ринат',  u'Магомед',  u'Ильдар',  u'Данил',  u'Аслан',  u'Рустем',  u'Ренат',  u'Ришат',
+                          u'Афанасий',  u'Гагик'])
+
+    return amplified
+
+
+def substitute_ye_for_yo(name):
+    substituted = ''
+    for char in name:
+        if char == u'ё':
+            substituted += u'е'
+        else:
+            substituted += char
+    return substituted
+
+
+def names_to_json(names, json_file):
+    with open(json_file, 'w') as handler:
+        json.dump(names, handler)
+
+
+def load_json(json_file):
+    with open(json_file) as data:
+        return json.load(data)
+
+
+if __name__ == '__main__':
+    # names_f = scrape_names(URL_F)
+    # names_to_json(names_f, NAMEBASE_F)
+    #
+    # names_m = scrape_names(URL_M)
+    # names_to_json(names_m, NAMEBASE_M)
+
+    # names_to_json(amplify_names(load_json(NAMEBASE_F), fem=True), NAMEBASE_F)
+    # names_to_json(amplify_names(load_json(NAMEBASE_M)), NAMEBASE_M)
+
+    pass
+
