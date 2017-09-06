dea742b88a44c73c12e7d713c58c146827d04401
sologuboved <sologuboved@gmail.com>
2017-09-06 19:42:21 +0300

    collected global variables into single file

---
+++ global_vars.py
@@ -0,0 +1,16 @@
+NAMEBASE_F = 'namebase_f.json'
+NAMEBASE_M = 'namebase_m.json'
+
+OLD_PE_NAMES_F = 'old_pe_names_f.json'
+OLD_PE_NAMES_M = 'old_pe_names_m.json'
+OLD_PE_UNCLEAR = 'old_pe_unclear.json'
+OLD_CONTACT_NAMES_F = 'old_contact_names_f.json'
+OLD_CONTACT_NAMES_M = 'old_contact_names_m.json'
+OLD_CONTACT_UNCLEAR = 'old_contact_unclear.json'
+
+NEW_PE_NAMES_F = 'new_pe_names_f.json'
+NEW_PE_NAMES_M = 'new_pe_names_m.json'
+NEW_PE_UNCLEAR = 'new_pe_unclear.json'
+NEW_CONTACT_NAMES_F = 'new_contact_names_f.json'
+NEW_CONTACT_NAMES_M = 'new_contact_names_m.json'
+NEW_CONTACT_UNCLEAR = 'new_contact_unclear.json'
